49145029889858f5927c318f0f0e91bdad9d7185
Vladislav Bogomolov <vladislav.bogomolov@eltex.loc>
2025-12-18 14:27:58 +0700

    unit tests

--- app/src/test/java/CalculatorClass.java
+++ app/src/test/java/CalculatorClass.java
@@ -1,2 +1,36 @@
-public class CalculatorClass {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.ValueSources;
+
+class CalculatorTest {
+    static Calculator calc;
+
+    @BeforeAll
+    static void createCalcInstance() {
+        calc = new Calculator();
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    void testPlus(Integer argument) {
+        Assertions.assertEquals(calc.plus.apply(argument, argument),
+                argument + argument);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 2, 3 })
+    void testMinus(Integer argument) {
+        Assertions.assertEquals(calc.minus.apply(argument, argument),
+                argument - argument);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 0, 1 })
+    void testDivideByZero(int argument) {
+        Assertions.assertThrowsExactly(
+                ArithmeticException.class,
+                () -> calc.devide.apply(argument, 0)
+        );
+    }
+
 }
